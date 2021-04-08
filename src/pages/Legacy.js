56837bf7c5b535ca6c623f2c302313ed6c551099
max-c-t-r <clever.max@hotmail.com>
2021-04-08 15:26:57 -0500

    primera parte de terminos y condiciones

--- src/pages/Legacy.js
+++ src/pages/Legacy.js
@@ -3,6 +3,20 @@ import React from 'react';
 const Legacy = () => (
   <div className='container'>
     <h2 className='container__title container__title--small'>Aviso Legal</h2>
+    <p>
+      Denominaciòn social: CURVIPS
+      <br />
+      Número de establecimineto: dadsadasd
+      <br />
+      Sucursal en Perú con NIF-0000000-h
+      <br />
+      E-mail: somos@galletas.es
+      <br />
+      Nombre de dominio:
+      <a href>
+        https://cur-vips.vercel.app/
+      </a>
+    </p>
   </div>
 );
 
