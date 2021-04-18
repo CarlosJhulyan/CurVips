b0e64347316d9b4ac6f9849de7f480968a956c12
CarlosJhulyan <cmedinallantirhuay@gmail.com>
2021-04-18 12:28:29 -0500

    iconos de sidebar mejorado, lista de los curriculos iniciando

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Menu } from 'antd';
 import { NavLink } from 'react-router-dom';
-import { AuditOutlined, UserOutlined, QuestionOutlined, SnippetsOutlined } from '@ant-design/icons';
+import { AuditOutlined, UserOutlined, QuestionOutlined, SnippetsOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import Logotipo from '../assets/images/curvips-logo.svg';
-import Logo from '../assets/images/logo.svg';
 
 function Sidebar() {
+  const [colllapse, setCollapse] = useState(false);
   return (
-    <div className='sidebar'>
+    <Menu className='sidebar' theme='dark' siderCollapsed={colllapse}>
+      <Button ghost className='sidebar__menu' icon={!colllapse ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />} onClick={() => setCollapse(!colllapse)} />
       <div className='sidebar-head'>
         <img className='sidebar-head__icon' src={Logotipo} alt='Logotipo de CurVips' />
-        <img className='sidebar-head__title' src={Logo} alt='Logo de Curvips' />
+        {!colllapse && 'Dashboard'}
       </div>
       <div className='sidebar-options'>
         <NavLink exact activeClassName='active-sidebar' to='/dashboard' className='sidebar-options__link'>
           <AuditOutlined className='sidebar-options__icon' />
-          Mis Currículos
+          {!colllapse && 'Mis Currículos'}
         </NavLink>
         <NavLink activeClassName='active-sidebar' to='/dashboard/account' className='sidebar-options__link'>
           <UserOutlined className='sidebar-options__icon' />
-          Gestionar Cuenta
+          {!colllapse && 'Gestionar Cuenta'}
         </NavLink>
         <NavLink activeClassName='active-sidebar' to='/dashboard/templates' className='sidebar-options__link'>
           <SnippetsOutlined className='sidebar-options__icon' />
-          Plantillas
+          {!colllapse && 'Plantillas'}
         </NavLink>
         <NavLink activeClassName='active-sidebar' to='/dashboard/help' className='sidebar-options__link'>
           <QuestionOutlined className='sidebar-options__icon' />
-          Preguntas Frecuentes
+          {!colllapse && 'Preguntas Frecuentes'}
         </NavLink>
       </div>
-    </div>
+    </Menu>
   );
 };
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { List, Space, Avatar } from 'antd';
+import { StarOutlined, LikeOutlined } from '@ant-design/icons';
+import Image from '../assets/images/templates/2021-04-04_18h39_17.png';
 
-const Dashboard = () => (
-  <div>Dashboard</div>
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
 );
+
+const Dashboard = () => {
+  const listData = [];
+  for (let i = 0; i < 23; i++) {
+    listData.push({
+      id: i,
+      title: `ant design part ${i}`,
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+      content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      liked: i + 12,
+      stars: i + 23,
+      image: Image,
+    });
+  }
+
+  return (
+    <List itemLayout='vertical' size='large' dataSource={listData} pagination={{ pageSize: 4 }} renderItem={(item) => <ItemList key={item.id} item={item} />} />
+  );
+};
+
+function ItemList({ item }) {
+  const { Item } = List;
+  return (
+    <Item actions={[<IconText icon={StarOutlined} text={item.stars} key='start' />, <IconText icon={LikeOutlined} text={item.liked} key='liked' />]}>
+      <Item.Meta avatar={<Avatar src={item.avatar} />} title={<a href={`/dashboard/cv/${item.title}`}>{item.title}</a>} description={item.description} />
+    </Item>
+  );
+}
 
 export default Dashboard;
