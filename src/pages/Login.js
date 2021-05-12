eb0375c76fcfad358413bbff06d2f97e58ae1efe
jhon.parco.27@unsch.edu.pe <jhon.parco.27@unsch.edu.pe>
2021-05-12 13:49:39 -0500

    este es un avanze,falta los logos o retoques aun

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,28 @@
+
 import React from 'react';
 
 const Login = () => (
-  <div className='LoginContainer'>Login</div>
+  <div id='panelLogin'>
+    <h1 className='let title1'>curvips</h1>
+    <div id='cuad'>
+      <p id='user-text' className='text let'>CORREO/USUARIO</p>
+      <input id='user' type='email' className='inputs' />
+      <p id='password-text' className='text let'>CONTRASEÑA</p>
+      <input id='password' type='password' className='inputs' />
+      <br />
+      <p className='let'>
+        <input type='checkbox' className='' />
+        Recordar mi usuario y contraseña
+      </p>
+      <button type='button' className='but let-buttom'>INGRESAR</button>
+      <div className='parte-register'>
+        <p className='let'>
+          ¿no tienes una cuenta?
+          <a href='Register' className=''>Registrate</a>
+        </p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Register = () => (
-  <div className='RegisterContainer'>Register</div>
+  <div id='panel'>
+    <h1 className='let title1'>curvips</h1>
+    <div id='cuad'>
+      <p id='user-text' className='text let'>USUARIO</p>
+      <input id='user' type='email' className='inputs' />
+      <p id='user-text' className='text let'>CORREO</p>
+      <input id='user' type='email' className='inputs' />
+      <p id='password-text' className='text let'>CONTRASEÑA</p>
+      <input id='password' type='password' className='inputs' />
+      <br />
+      <p className='let vol'>
+        <input type='checkbox' />
+        Aceptar los
+        <a href='https://www.youtube.com'>Terminos de servicio</a>
+      </p>
+      <button type='button' className='but let-buttom'>REGISTRARSE</button>
+      <div className='parte-register2'>
+        <p className='let'>
+          ¿ya tienes una cuenta?
+          <a href='Login' className=''>Ingresa aqui</a>
+        </p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Register;
